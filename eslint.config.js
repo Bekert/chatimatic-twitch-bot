39c5c9f3fea8d6b33403bca75fad8423d1fd269a
Bekert <moroz.bogdan@protonmail.com>
2024-05-18 18:16:00 +0300

    refactor: typescript migration

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,27 @@
-import babelParser from '@babel/eslint-parser'
 import js from '@eslint/js'
-import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default [
+export default tseslint.config(
 	js.configs.recommended,
+	...tseslint.configs.recommended,
 	{
-		languageOptions: {
-			parser: babelParser,
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true
-				}
-			},
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
+		ignores: ['built/']
+	},
+	{
+		plugins: {
+			'@stylistic': stylistic
 		},
 		rules: {
-			'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-			semi: ['error', 'never'],
-			'max-len': ['warn', { code: 100 }],
-			quotes: ['error', 'single', { avoidEscape: true }],
-			'jsx-quotes': ['error', 'prefer-single'],
-			'quote-props': ['error', 'as-needed'],
-			'comma-dangle': ['error', 'never'],
-			'object-curly-spacing': ['error', 'always'],
-			'array-bracket-spacing': ['error', 'never'],
-			'arrow-parens': ['error', 'as-needed']
-		},
-		ignores: ['dist', 'build', 'node_modules']
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'@stylistic/semi': ['error', 'never'],
+			'@stylistic/max-len': ['warn', { code: 100 }],
+			'@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+			'@stylistic/quote-props': ['error', 'as-needed'],
+			'@stylistic/comma-dangle': ['error', 'never'],
+			'@stylistic/object-curly-spacing': ['error', 'always'],
+			'@stylistic/array-bracket-spacing': ['error', 'never'],
+			'@stylistic/arrow-parens': ['error', 'as-needed']
+		}
 	}
-]
+)
